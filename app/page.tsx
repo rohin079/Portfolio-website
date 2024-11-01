0d4ce48171e6a4a32f17c9de48a5766042dde8c4
rohin079 <rohinmehrotra@gmail.com>
2024-11-01 22:12:57 +0530

    feat: initialises project with next and shadcn

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { GridBackground } from "@/components/gridBackground";
+
+export default function Home() {
+  return (
+    <div className="dark">
+
+    <GridBackground></GridBackground>
+    </div>
+    
+  );
+}
---
+++ types/tailwindcss-modules.d.ts
@@ -0,0 +1,5 @@
+declare module 'tailwindcss/lib/util/flattenColorPalette' {
+    const flattenColorPalette: (colors: any) => Record<string, string>;
+    export default flattenColorPalette;
+  }
+  
